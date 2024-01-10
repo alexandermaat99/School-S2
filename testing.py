3c2b78866c1e32e7442ae79ee777f27283abc680
Alexander Maat <107603244+alexandermaat99@users.noreply.github.com>
2024-01-10 09:50:55 -0700

    testingWorks

--- testing.py
+++ testing.py
@@ -1,1 +1,8 @@
-print("testing")
+import pandas as pd
+
+file_path = '/Users/alexandermaat/Desktop/School/NBA/nba_2022-23_all_stats_with_salary.csv'
+df = pd.read_csv(file_path)
+print(df.head(1))
+
+
+ 
